3706eed96e2c0c318537759447c0e4e62469dbaa
angpy43 <anganpymain@gmail.com>
2026-05-29 21:00:00 +0000

    what changed

---
+++ basicserver/client.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+int main()
+{
+
+    int client_fd;
+    client_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_fd < 0)
+    {
+        perror("socket");
+        return 1;
+    }
+    printf("Client socket created!\n");
+
+    struct sockaddr_in server_addr = {0};
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(8080);
+    inet_pton(AF_INET,
+              "127.0.0.1",
+              &server_addr.sin_addr);
+
+    if (connect(client_fd,
+                (struct sockaddr *)&server_addr,
+                sizeof(server_addr)) < 0)
+    {
+        perror("connect");
+        return 1;
+    }
+
+    printf("Connected to server!\n");
+
+    close(client_fd);
+
+    return 0;
+}
