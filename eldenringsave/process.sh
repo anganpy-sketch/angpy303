c5b3e125d5d9a8d2d016baaf28513ad8c9c0917b
angpy43 <anganpymain@gmail.com>
2026-05-30 06:03:22 +0000

    what changed

--- eldenringsave/process.sh
+++ eldenringsave/process.sh
@@ -1,6 +1,10 @@
 while true
 do 
 	if tasklist.exe|grep -iq "ChatGPT.exe"
+	#chatgpt is example here use elden ring instead.
+	#use your save and backfiles instead of txt files.
+	#will update to full working version later.
+
 	then cp backup1.txt testfile.txt
 	     echo "Done sucesfully"
 	     break
